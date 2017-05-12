0e3ba65aca882d074f176816bf3e6fc510edc28c
Isabella Abatgis <abatgis@gmail.com>
2017-05-12 12:15:32 -0700

    add functionality to calculate price and display order

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,23 @@
-//business logic
+// business logic
 // function Order(pizza, price) {
-//   this.pizza = [];
+//   this.pizza = pizza;
 //   this.price = price;
 // }
 
-function Pizza(size, toppings, special, price) {
+function Pizza(size, toppings, special) {
   this.size = size;
   this.toppings = toppings;
   this.special = special;
-  this.price = price;
 }
 
 
-// Pizza.prototype.getPrice = function(newPrice) {
-//   var result = 0;
-//   for (var i = 0; i < newPrice.length; i++) {
-//     newPrice += (i * 1.25);
-//   }
-//   this.price = result;
-// }
-
-// Pizza.prototype.getToppings = function() {
-//   return this.type + ", " + this.street + ", " + this.city + ", " + this.state;
-// }
+Pizza.prototype.getPrice = function() {
+  var newPrice = parseFloat(this.size);
+  for (var i = 0; i < this.toppings.length; i++) {
+    newPrice += (1.25);
+  }
+  return newPrice;
+}
 
 // function resetFields() {
 //     $("input#new-first-name").val("");
@@ -52,36 +47,35 @@ $(document).ready(function() {
         checkedSpecial.push(pushSpecial);
       });
 
-    var newPrice = (parseFloat(checkedSize) + ((checkedToppings.length) * 1.25))
 
-    var newPizza = new Pizza (checkedSize, checkedToppings, checkedSpecial, newPrice);
+    var newPizza = new Pizza (checkedSize, checkedToppings, checkedSpecial);
+
+    var pizzaPrice = newPizza.getPrice();
 
 
-    console.log(newPizza);
+    if (newPizza.size === "9.30") {
+      $("#pizza-size").text("Small");
+    }
+    else if (newPizza.size === "11.60") {
+      $("#pizza-size").text("Medium");
+    }
+    else if (newPizza.size === "14.45") {
+      $("#pizza-size").text("Large");
+    }
+    else if (newPizza.size === "17.35") {
+      $("#pizza-size").text("X-Large");
+    }
 
-    // $(".new-address").each(function() {
-    //   var inputtedType = $(this).find("input.new-type").val();
-    //   var inputtedStreet = $(this).find("input.new-street").val();
-    //   var inputtedCity = $(this).find("input.new-city").val();
-    //   var inputtedState = $(this).find("input.new-state").val();
-    //   var newAddress = new Address(inputtedType, inputtedStreet, inputtedCity, inputtedState);
-    //   newContact.addresses.push(newAddress);
-    // });
-    //
-    // $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-    //
-    // $(".newer-address").remove();
-    //
-    // $(".contact").last().click(function() {
-    //   $("#show-contact").show();
-    //   $("#show-contact h2").text(newContact.firstName);
-    //   $(".first-name").text(newContact.firstName);
-    //   $(".last-name").text(newContact.lastName);
-    //   $("ul#addresses").text("");
-    //   newContact.addresses.forEach(function(address) {
-    //     $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-    //   });
-    // });
+    for (var i = 0; i < newPizza.toppings.length; i++) {
+      $("ul#pizza-toppings").append("<li>" + newPizza.toppings[i] + "</li>");
+    }
+
+    for (var i = 0; i < newPizza.special.length; i++) {
+      $("ul#pizza-specials").append("<li>" + newPizza.special[i] + "</li>");
+    }
+
+    $("#pizza-price").text("$" + pizzaPrice);
+
 
     // resetFields();
 
